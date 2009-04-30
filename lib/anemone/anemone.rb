efacd9cc120f167c6bf94374b903e1dfce0a5319
Chris Kite <chris.kite@vertive.com>
2009-04-30 11:27:11 -0500

    increased version to 0.0.2

--- lib/anemone/anemone.rb
+++ lib/anemone/anemone.rb
@@ -3,7 +3,7 @@ require 'anemone/core'
 
 module Anemone
   # Version number
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
   
   # User-Agent string used for HTTP requests
   USER_AGENT = "Anemone/#{self::VERSION}"
