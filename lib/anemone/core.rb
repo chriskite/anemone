4b0ff55430c704709856c0026616e6a67148b273
Yvan Aillet <yvanaillet@yahoo.fr>
2012-04-24 10:09:42 +0200

    NTLM parameters in core.rb

--- lib/anemone/core.rb
+++ lib/anemone/core.rb
@@ -56,6 +56,14 @@ module Anemone
       :proxy_port => false,
       # HTTP read timeout in seconds
       :read_timeout => nil
+      # Are we using NTLM protocol ?
+      :use_ntlm => false
+      # NTLM user name
+      :ntlm_user => nil
+      # NTLM domain name
+      :ntlm_domain => nil
+      # NTLM password
+      :ntlm_password => nil
     }
 
     # Create setter methods for all options to be called from the crawl block
